f229861aeb2192cab46582ac7943fb0b8729fd1c
JoãoPolloni <jpolloni16@gmail.com>
2023-10-02 15:01:14 -0300

    Prova

--- Elixir/higher-order-function.exs
+++ Elixir/higher-order-function.exs
@@ -1,7 +1,7 @@
 # Funções de order superior são funções que recebem outras funções como argumento ou retornam funções como resultado.
 # Em outras palavras, são funções que operam em outras funções, seja tomando-as como argumentos ou retornando-as.
 # O fato de você conseguir passar funções como argumentos para outras funções é uma das características mais poderosas do Elixir.
-#  Porque isso permite que você crie funções que são extremamente flexíveis e reutilizáveis.
+# Porque isso permite que você crie funções que são extremamente flexíveis e reutilizáveis.
 
 
  defmodule Math do
